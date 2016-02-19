0e80e66800bc455a5728059b6e3d7f1458bac6cf
Fausto De La Torre <faustodelatog@gmail.com>
2016-02-18 20:35:38 -0500

    solo los usuarios administradores pueden acceder a la seccion de admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,9 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
-  before_action :authenticate_usuario!
+  before_action :authenticate_usuario!, :authorize_admin
   #before_filter :login_required, :load_admin_menu
 
-  def index
+  def authorize_admin
+    redirect_to root_path, alert: 'Access Denied' unless current_usuario.isAdmin?
   end
 end
