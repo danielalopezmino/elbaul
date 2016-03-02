c78edb4c7a25a0673f22e659dc24dd8cbd89dfac
Fausto De La Torre <faustodelatog@gmail.com>
2016-03-01 21:42:40 -0500

    envia mails para recuperar la contraseña

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,19 @@ Rails.application.configure do
 
   #devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,20 @@ Rails.application.configure do
 
   #devise
   config.action_mailer.default_url_options = { host: 'elbaul.heroku.com', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
