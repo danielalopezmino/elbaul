b18eb1be229be32a86a7d0184c66ae6f979e2e2a
Fausto De La Torre <faustodelatog@gmail.com>
2016-03-01 22:49:06 -0500

    removido el puerto para el envio de correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #devise
-  config.action_mailer.default_url_options = { host: 'maleboxgifts.heroku.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'maleboxgifts.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
