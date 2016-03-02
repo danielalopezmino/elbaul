6a8c50183c14fcaf2a07729a97cfbce6bbfb74f4
Fausto De La Torre <faustodelatog@gmail.com>
2016-03-01 22:21:28 -0500

    aplicacion renombrada a maleboxgifts

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'recaptcha'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Elbaul
+module Maleboxgifts
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
